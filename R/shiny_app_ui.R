4c0658564705cbb70f51721a5a12e1fda638f3eb
L. Fehrmann <L.Fehrmann@UFFE35.uni-forst.gwdg.de>
2019-05-24 08:47:30 +0200

    Order of tabpanels changed

--- R/shiny_app_ui.R
+++ R/shiny_app_ui.R
@@ -9,21 +9,6 @@ shiny_app_ui <- fluidPage(
     "Global Sampling Grid",
     id = "nav",
 
-    tabPanel(
-      "Interactive map",
-      value = "gomap",
-
-      div(
-        class = "outer",
-        tags$head(# Include our custom CSS
-          includeCSS("inst/shiny_app/styles.css")
-        ),
-
-        leafletOutput("map", height = "100%", width = "100%")
-      )
-    ),
-
-
     tabPanel("Generate/ download GSG",
              div(id = "settings",
                  fluidRow(
@@ -122,6 +107,19 @@ shiny_app_ui <- fluidPage(
              plotOutput("se_plot", width = 450)
     ),
 
+    tabPanel(
+      "Interactive map",
+      value = "gomap",
+
+      div(
+        class = "outer",
+        tags$head(# Include our custom CSS
+          includeCSS("inst/shiny_app/styles.css")
+        ),
+
+        leafletOutput("map", height = "100%", width = "100%")
+      )
+    ),
 
     tabPanel("Data Explorer",
              DT::dataTableOutput('mytable'))
